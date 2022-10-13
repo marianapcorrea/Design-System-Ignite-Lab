426c7d891c1d4705382ca071f458263ec32e4716
Mariana Correa <mpcs270391@gmail.com>
2022-10-13 17:34:38 -0300

    Update storybook e package.json for assessibility tool

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -4,6 +4,7 @@ module.exports = {
         "@storybook/addon-links",
         "@storybook/addon-essentials",
         "@storybook/addon-interactions",
+        "@storybook/addon-a11y",
     ],
     framework: "@storybook/react",
     core: {
@@ -11,5 +12,5 @@ module.exports = {
     },
     features: {
         storyStoreV7: true,
-    }
+    },
 };
