12a6cbb3f222da59f01d5d7534ef2098db39c35c
Mariana Corrêa <73893586+marianapcorrea@users.noreply.github.com>
2022-10-13 16:13:37 -0300

    Update main.cjs

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -11,12 +11,5 @@ module.exports = {
     },
     features: {
         storyStoreV7: true,
-    },
-    viteFinal: (config, { configType }) => {
-        if (configType === "PRODUCTION") {
-            config.base = "/ignite-lab-design-system/";
-        }
-
-        return config;
-    },
+    }
 };
