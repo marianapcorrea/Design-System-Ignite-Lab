fb962590e9195ae771b785e8d27fbc2e8907b4fe
Mariana Correa <mpcs270391@gmail.com>
2022-10-12 21:10:39 -0300

    Cria componente de texto

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import './styles/global.css';
 export function App() {
 
     return (
-        <h1 className="font-bold text-5xl  text-violet-500">Hello World!</h1>
+        <>
+            <h1 className=" font-bold text-2xl text-gray-800 ">Hello World!</h1>
+            <button className="bg-cyan-500 text-gray-400 font-medium px-4 py-2 ">Enviar</button>
+        </>
     )
 }
 
